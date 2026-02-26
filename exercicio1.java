e2ae6046505f529795bab50d59be7e3572f52371
queien <gqbrielfmachado@gmail.com>
2026-02-25 21:23:09 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,10 @@
+class exercicio1 {
+	
+	public static void main (String[] args) {
+		float value1 = 10;
+		float porcent1 = value1/10;
+		float result1 = value1 + porcent1;
+		System.out.println(value1);
+		System.out.println(result1);
+	}
+}
