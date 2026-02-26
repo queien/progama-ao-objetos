293b27c4251b8f7417ce211debbfa75e4c1470e5
queien <gqbrielfmachado@gmail.com>
2026-02-25 21:23:41 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Exercicio2 {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int presenca = sc.nextInt();
+		int porcent = (presenca * 100) /120;
+		System.out.println(porcent);
+		
+	}
+}
+
